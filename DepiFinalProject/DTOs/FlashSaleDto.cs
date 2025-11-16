03dfb97836a656f28c9ec7a0eca83ad858568149
IsraaYasserHussein <esraayasser448@gmail.com>
2025-11-16 23:01:50 +0200

    added flash sale endpoints

---
+++ DepiFinalProject/DTOs/FlashSaleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DepiFinalProject.DTOs
+{
+    public class FlashSaleDto
+    {
+        public int FlashSaleID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public int? MaxUsers { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
---
+++ DepiFinalProject/Interfaces/IFlashSaleService.cs
@@ -0,0 +1,13 @@
+﻿using DepiFinalProject.DTOs;
+
+namespace DepiFinalProject.Services
+{
+    public interface IFlashSaleService
+    {
+        Task<IEnumerable<FlashSaleDto>> GetAllAsync();
+        Task<FlashSaleDto?> GetByIdAsync(int id);
+        Task<FlashSaleDto> CreateAsync(CreateFlashSaleDto dto);
+        Task<FlashSaleDto?> UpdateAsync(int id, UpdateFlashSaleDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
